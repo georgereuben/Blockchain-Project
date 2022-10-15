0558532675b16adc12ef326adb46600a6e426072
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2022-10-15 18:47:48 +0530

    Transactions according to indices

--- nidhi_ws.py
+++ nidhi_ws.py
@@ -1,3 +1,77 @@
+import hashlib
+import json
+from collections import OrderedDict
+
+
+# 1. Declare the class trees
+class Jae_MerkTree:
+
+	# 2. Initiate the class object
+	def __init__(self,listoftransaction=None):
+		self.listoftransaction = listoftransaction
+		self.past_transaction = OrderedDict()
+
+	# 3. Create the Merkle Tree  
+	def create_tree(self):
+
+		# 3.0 Continue on the declaration
+		listoftransaction = self.listoftransaction
+		past_transaction = self.past_transaction
+		temp_transaction = []
+
+		# 3.1 Loop until the list finishes
+		for index in range(0,len(listoftransaction),2):
+
+			# 3.2 Get the most left element 
+			current = listoftransaction[index]
+
+			# 3.3 If there is still index left get the right of the left most element
+			if index+1 != len(listoftransaction):
+				current_right = listoftransaction[index+1]
+
+			# 3.4 If we reached the limit of the list then make a empty string
+			else:
+				current_right = ''
+
+			# 3.5 Apply the Hash 256 function to the current values
+			current_hash = hashlib.sha256(current.encode('utf-8'))
+
+			# 3.6 If the current right hash is not a '' <- empty string
+			if current_right != '':
+				current_right_hash = hashlib.sha256(current_right.encode('utf-8'))
+
+			# 3.7 Add the Transaction to the dictionary 
+			past_transaction[listoftransaction[index]] = current_hash.hexdigest()
+
+			# 3.8 If the next right is not empty
+			if current_right != '':
+				past_transaction[listoftransaction[index+1]] = current_right_hash.hexdigest()
+
+			# 3.9 Create the new list of transaction
+			if current_right != '':
+				temp_transaction.append(current_hash.hexdigest() + current_right_hash.hexdigest())
+
+			# 3.01 If the left most is an empty string then only add the current value
+			else:
+				temp_transaction.append(current_hash.hexdigest())
+
+		# 3.02 Update the variables and rerun the function again 
+		if len(listoftransaction) > 1:
+			self.listoftransaction = temp_transaction
+			self.past_transaction = past_transaction
+
+			# 3.03 Call the function repeatly again and again until we get the root 
+			self.create_tree()
+
+	# 4. Return the past Transaction 
+	def Get_past_transacion(self):
+		return self.past_transaction
+
+	# 5. Get the root of the transaction
+	def Get_Root_leaf(self):
+		last_key = list(self.past_transaction.keys())[-1]
+		return self.past_transaction[last_key]
+
 class property:
     def __init__(self, pid, price, owner):
         self.pid = pid
@@ -76,7 +150,6 @@ def enter_transaction():
     flag = 0
     for i in user_list:
         if(i.uid == buyer):
-            b_index = i
             flag = 1
             break
     if(not flag):
@@ -84,9 +157,8 @@ def enter_transaction():
         return
     
     flag = 0
-    for i in range (0, user_list.size()):
-        if(user_list.uid == seller):
-            s_index = i
+    for i in user_list:
+        if(i.uid == seller):
             flag = 1
             break
     if(not flag):
@@ -94,37 +166,51 @@ def enter_transaction():
         return
     
     flag = 0
-    for i in range(0, property_list.size()):
-        if(property_list[i].pid == pid):
+    ctr=-1
+    for i in property_list:
+        ctr+=1
+        if(i.pid == pid):
             flag = 1
-            p_index = i
             break
-        if(not flag):
-            print("Property not found")
-            return
-
-    if(property_list[p_index].owner != seller):
+    if(not flag):
+        print("Property not found")
+        return
+    
+    if(property_list[ctr].owner != seller):
         print("Seller is not the owner of the property")
         return
 
-    if(property_list[p_index].price > user_list[b_index].wallet):
+    if(property_list[ctr].price > user_list[buyer].wallet):
         print("Buyer does not have enough money")
         return
 
     # add more testcases
 
-    property_list[p_index].owner = buyer
+    property_list[pid].owner = buyer
 
-    user_list[b_index].ustake += property_list[p_index].price
-    user_list[s_index].ustake -= property_list[p_index].price
+    user_list[buyer].ustake += property_list[pid].price
+    user_list[seller].ustake -= property_list[pid].price
 
-    user_list[b_index].wallet -= property_list[p_index].price
-    user_list[s_index].wallet += property_list[p_index].price
+    user_list[buyer].wallet -= property_list[pid].price
+    user_list[seller].wallet += property_list[pid].price
 
     return transaction(buyer, seller, pid)
 
 def calculate_hashes():
-    pass
+    Jae_Tree = Jae_MerkTree()
+    Jae_Tree.listoftransaction = transaction_list
+    Jae_Tree.create_tree()
+    past_transaction = Jae_Tree.Get_past_transacion()
+    print ('Final root of the tree : ',Jae_Tree.Get_Root_leaf())
+	# c) pass on the transaction list 
+	
+	# d) Create the Merkle Tree transaction
+	
+	# e) Retrieve the transaction 
+	
+	# f) Get the last transaction and print all 
+	
+    return
 
 def view_transaction_history(p):
     ctr = 0
---
+++ pos.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+import time
+from hashlib import sha256
+import json
+from random import randint
+
